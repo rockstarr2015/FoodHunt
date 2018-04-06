30ab4368a0a21acd868b3e5cf857ba0a8b888ddb
rockstarr2015 <amore1@umassd.edu>
2018-04-06 12:02:52 -0400

    googlemap and marker animation

--- www/js/index.js
+++ www/js/index.js
@@ -25,7 +25,225 @@ var initMap = function(){
 
         var mapOptions = {
             center: {lat:location.latitude , lng: location.longitude},
-            zoom:  16
+            zoom:  14,
+            styles:[
+                      {
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#ebe3cd"
+                          }
+                        ]
+                      },
+                      {
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#523735"
+                          }
+                        ]
+                      },
+                      {
+                        "elementType": "labels.text.stroke",
+                        "stylers": [
+                          {
+                            "color": "#f5f1e6"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "administrative",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#c9b2a6"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "administrative.land_parcel",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#dcd2be"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "administrative.land_parcel",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#ae9e90"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "landscape.natural",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#dfd2ae"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#dfd2ae"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#93817c"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi.park",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                          {
+                            "color": "#a5b076"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi.park",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#447530"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#f5f1e6"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.arterial",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#fdfcf8"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#f8c967"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#e9bc62"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway.controlled_access",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#e98d58"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway.controlled_access",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#db8555"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.local",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#806b63"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit.line",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#dfd2ae"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit.line",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#8f7d77"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit.line",
+                        "elementType": "labels.text.stroke",
+                        "stylers": [
+                          {
+                            "color": "#ebe3cd"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit.station",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#dfd2ae"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                          {
+                            "color": "#b9d3c2"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#92998d"
+                          }
+                        ]
+                      }
+                    ],
+            disableDefaultUI: true
+
+
         };
 
         var pyrmont = new google.maps.LatLng(location.latitude,location.longitude);
@@ -41,9 +259,13 @@ var initMap = function(){
         var service = new google.maps.places.PlacesService(map);
         service.textSearch(request, (result,status)=>{
             if (status == google.maps.places.PlacesServiceStatus.OK) {
+
                 //creating marker 
-                result.forEach(function(d){
-                    createMarker(d);
+                result.forEach(function(data,index){
+                    setTimeout(function(){
+                        createMarker(data);
+                    }, index*200)
+                    
                 })
               }
         });
@@ -69,16 +291,23 @@ var initMap = function(){
                 content: contentString
             });
 
-            console.log(infowindow);
+
+            
 
             var marker = new google.maps.Marker({
                 position: myLatLng,
-                map: map,
-                title: 'Hello World!'
+                map: map, 
+                animation: google.maps.Animation.DROP,
+                title: data.name
             });
           
-            marker.addListener('click', function() {
-            infowindow.open(map, marker);
+            //adding markert to map
+            marker.addListener('click', function(event) {
+                
+                console.log("marker is clicked...");
+                //display the data 
+                displayInfo(data);
+               
             });
             
         }
@@ -86,9 +315,14 @@ var initMap = function(){
 
     }
 
-    
 
 
+}
+
+var displayInfo = function(data){
+
+    console.log(data.name);
+    console.log("displaying marker information");
 }
 
 
